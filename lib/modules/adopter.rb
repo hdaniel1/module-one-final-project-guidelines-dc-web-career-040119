5023eabf83c30280dc8b98a03e666330a96274b5
Daniel Hallada <hallada.daniel@gmail.com>
2019-04-15 16:12:09 -0400

    edit

--- lib/modules/adopter.rb
+++ lib/modules/adopter.rb
@@ -1,4 +1,5 @@
 class Adopter < ActiveRecord::Base
 	has_many :preferences
+	has_many :favorite_pets
 	has_many :pets, through: :favorite_pets
 end 
--- lib/modules/pet.rb
+++ lib/modules/pet.rb
@@ -1,5 +1,6 @@
 class Pet < ActiveRecord::Base
 	belongs_to :shelter
+	has_many :favorite_pets
 	has_many :adopters, through: :favorite_pets
 
 end 
