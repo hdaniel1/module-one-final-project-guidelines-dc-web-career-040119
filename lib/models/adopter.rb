aeff604746cedb67c3c5ed0be36a8ca43e20041f
Matthew Lindsey <mbl@null.net>
2019-04-17 13:21:00 -0400

    added basic show_available_pets method

--- lib/models/adopter.rb
+++ lib/models/adopter.rb
@@ -1,5 +1,5 @@
 
-class Adopter < ActiveRecord::Base 
+class Adopter < ActiveRecord::Base
 	has_many :favorite_pets
 	has_many :pets
 	has_many :pets, through: :favorite_pets
@@ -7,7 +7,7 @@ class Adopter < ActiveRecord::Base
 	#gets the users fullname
 	def full_name
 		"#{self.first_name} #{self.last_name}"
-	end 
+	end
 
 	#present menu options
 	def present_options
@@ -18,18 +18,20 @@ class Adopter < ActiveRecord::Base
 		puts "3. View my favorite pets"
 		puts "4. Logout"
 		self.choose_menu_option
-	end 
+	end
 
 	#menu options paths
-	def choose_menu_option 
+	def choose_menu_option
 		response = gets.chomp
 		if response == "1"
 			self.show_preferences
+    elsif response == "2"
+      self.show_available_pets
 		elsif response == "4"
 			puts "Goodbye"
 			$user = nil
 		end
-	end 	
+	end
 
 	#shows user's existing preferences
 	def show_preferences
@@ -40,11 +42,11 @@ class Adopter < ActiveRecord::Base
 		puts "To update your preferences, enter 'change'. Otherwise, enter 'exit' to return to the main menu"
 		self.change_preferences
 		self.save
-	end 
+	end
 
 	#goes through methods to update preferences
 	def change_preferences
-		loop do 
+		loop do
 		response = gets.chomp
 		if response == "change"
 			self.set_preferred_species
@@ -55,10 +57,10 @@ class Adopter < ActiveRecord::Base
 		elsif response =='exit'
 			self.present_options
 			break
-		else 
+		else
 			puts "Please enter a valid response"
 		end
-		end 
+		end
 	end
 
 	#wording for updating preferred_species
@@ -69,18 +71,18 @@ class Adopter < ActiveRecord::Base
 		puts "2. Cats"
 		puts "3. Rabbits"
 		puts "Please enter the number corresponding to the species you're interested in"
-		self.parse_species	
-	end 
+		self.parse_species
+	end
 
 	#wording for updating preferred_temperament
-	def set_preferred_temperament 
+	def set_preferred_temperament
 		puts "Please choose from one of the following temperaments"
 		puts "1. Outgoing"
 		puts "2. Quiet"
 		puts "3. Independent"
 		self.parse_temperament
 		self.save
-	end 
+	end
 
 	#wording for updating preferred_size
 	def set_preferred_size
@@ -93,49 +95,46 @@ class Adopter < ActiveRecord::Base
 	end
 
 	#logic for defining user response for preferred species
-	def parse_species 
-		preferred_species = []
-		valid_responses = ["1", "2", "3"]
-		loop do 
+	def parse_species
+    species = nil
+
+		loop do
 
 		response = gets.chomp
 		#make sure response contains appropriate values
-		if (response.split.uniq - valid_responses).empty?
-			#pushes corresponding species into array
-			response.split.uniq.each {|i|
-				if i == "1"
-					preferred_species << "Dogs"
-				elsif i == "2"
-					preferred_species << "Cats"
-				elsif i == "3"
-					preferred_species << "Rabbits"
-				end 	
-			}
-			puts  "You chose #{preferred_species.uniq}. If this is correct, enter 'done'. Otherwise, please re-enter your choices."
-		#finalizes selection
-		elsif response.downcase == "done"
-			self.preferred_species = "#{preferred_species}"
-			puts "Thank you for your selection"
-			break #return to homepage here
-		#quits selection
+		if response == "1"
+			species = "dog"
+			puts "You chose #{species}. If this is correct, enter 'done'. Otherwise, please re-enter your choice."
+		elsif response == "2"
+			species = "cat"
+			puts "You chose #{species}. If this is correct, enter 'done'. Otherwise, please re-enter your choice."
+		elsif response == "3"
+			species = "rabbit"
+			puts "You chose #{species}. If this is correct, enter 'done'. Otherwise, please re-enter your choice."
+		elsif response == "done"
+			if species.nil? == false
+				self.preferred_species = species
+				puts "Thank you for your selection"
+			else
+				puts "Thank you for your selection"
+			end
+			break
 		elsif response.downcase == "quit"
-			preferred_species = []
 			puts "Goodbye"
-			break #return to homepage here
-		#invalid response
+			break
 		else
 			puts "Invalid response - to quit, type 'quit'"
-		end 
+		end
 
 		end #ends the loop
-	end 
+	end
 
 	#logic for defining user response for preferred temperament
-	def parse_temperament 
+	def parse_temperament
 
 		temperament = nil
 
-		loop do 
+		loop do
 
 		response = gets.chomp
 		#make sure response contains appropriate values
@@ -155,23 +154,23 @@ class Adopter < ActiveRecord::Base
 			else
 				puts "Thank you for your selection"
 			end
-			break 
+			break
 		elsif response.downcase == "quit"
 			puts "Goodbye"
 			break
-		else 
+		else
 			puts "Invalid response - to quit, type 'quit'"
-		end 
+		end
 
 		end #ends the loop
 	end
 
 	#logic for defining user response for preferred size
-	def parse_size 
+	def parse_size
 
 		size = nil
 
-		loop do 
+		loop do
 
 		response = gets.chomp
 		#make sure response contains appropriate values
@@ -191,15 +190,33 @@ class Adopter < ActiveRecord::Base
 			else
 				puts "Thank you for your selection"
 			end
-			break 
+			break
 		elsif response.downcase == "quit"
 			puts "Goodbye"
 			break
-		else 
+		else
 			puts "Invalid response - to quit, type 'quit'"
-		end 
+		end
 
 		end #ends the loop
 	end
 
-end 
+  def show_available_pets
+    self.preferred_species
+    self.preferred_temperament
+    self.preferred_size
+
+    where_statement = [
+      "species = ? and temperament = ? and size = ?",
+      self.preferred_species.downcase,
+      self.preferred_temperament.downcase,
+      self.preferred_size.downcase
+    ]
+
+    p where_statement
+    p Pet.where(where_statement)
+
+
+  end
+
+end
--- lib/run_methods.rb
+++ lib/run_methods.rb
@@ -4,9 +4,9 @@ def welcome
 
 	puts "Welcome to [Insert placeholder name here]"
 	puts "Are you an existing user? Yes/No"
-	
-	loop do 
-	
+
+	loop do
+
 	response = gets.chomp
 	if response.downcase == "yes"
 		login
@@ -26,26 +26,26 @@ def welcome
 		puts "Have a nice day!"
 		break
 	else puts "Please enter Yes or No for login access - enter quit to close application"
-	end 
+	end
 
-	end 
-end 
+	end
+end
 
 #go through account creation process
 def create_account
 	puts "Please tell us your first name"
-	first_name = gets.chomp 
+	first_name = gets.chomp
 
 	puts "Please tell us your last_name"
 	last_name = gets.chomp
 
 	puts "Please provide a username for login access"
-	loop do 
+	loop do
 	username = gets.chomp
 
 	if Adopter.find_by(username:username)
 		puts "We're sorry, that username already exists. Please try another"
-	else 
+	else
 		puts "Thanks for joining!"
 		Adopter.create(username: username, first_name: first_name, last_name: last_name)
 		# binding.pry
@@ -53,30 +53,28 @@ def create_account
 		break
 	end
 	end
-end 
+end
 
 #search for exising username to log in with
 def login
 
 	puts "Please enter your username to log in "
-	
-	loop do 
+
+	loop do
 
 	response = gets.chomp
 
-	if Adopter.find_by(username:response) 
+	if Adopter.find_by(username:response)
 		puts "Welcome back!"
 		$user = Adopter.find_by(username:response)
-		break 
+		break
 	elsif response.downcase == "exit"
 		puts "Goodbye"
 		break
-	else 
+	else
 		puts "Hmmm, we couldn't find an existing account with that username. Please try again. To exit, please enter Exit"
-	end 
+	end
 
-	end 
+	end
 
-end 
-
-
+end
